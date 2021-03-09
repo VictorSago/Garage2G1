839bfde28fac9591d589833a8cc5a69b7998c119
K-J <kj.ortigoza@gmail.com>
2021-03-09 14:11:18 +0100

    nice2

--- Garage2G1/Program.cs
+++ Garage2G1/Program.cs
@@ -15,6 +15,8 @@ namespace Garage2G1
         {
             CreateHostBuilder(args).Build().Run();
             Console.WriteLine("nice");
+
+            Console.WriteLine("nice2");
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
