d69ad88708d931e42f8f7017877de7a973a70cfb
K-J <kj.ortigoza@gmail.com>
2021-03-10 11:49:15 +0100

    Modes/ParkedVehicle() +added some validation

--- Garage2G1/Models/ParkedVehicle.cs
+++ Garage2G1/Models/ParkedVehicle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,12 +15,21 @@ namespace Garage2G1.Models
         }
 
         public int Id { get; set; }
+
+        [Range(1, 5)]
         public VehicleType VehicleType { get; set; }
+        [Required(AllowEmptyStrings = false)]
         public string RegNumber { get; set; }
+        [Required(AllowEmptyStrings = false)]
         public string Color { get; set; }
+        [Required(AllowEmptyStrings = false)]
         public string Brand { get; set; }
+        [Required(AllowEmptyStrings = false)]
         public string Model { get; set; }
+
+        [Range(1, int.MaxValue)]
         public int NumberOfWheels { get; set; }
+        [Display(Name = "Order Date")]
         public DateTime ArrivalTime { get; private set; }
 
     }
