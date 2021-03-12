20a9124242e7dcd41ab505300c5ca66997b81557
Victor Sago <VictorSago01@gmail.com>
2021-03-12 12:58:33 +0100

    Transition back to sqlite

--- Garage2G1/Startup.cs
+++ Garage2G1/Startup.cs
@@ -30,12 +30,12 @@ namespace Garage2G1
             services.AddControllersWithViews();
 
             // SQLServer option
-            services.AddDbContext<ParkedVehicleContext>(options => 
-                    options.UseSqlServer(Configuration.GetConnectionString("ParkedVehicleContext")).EnableSensitiveDataLogging());
+            // services.AddDbContext<ParkedVehicleContext>(options => 
+            //         options.UseSqlServer(Configuration.GetConnectionString("ParkedVehicleContext")).EnableSensitiveDataLogging());
 
             // SQLite option
-            // services.AddDbContext<ParkedVehicleContext>(options => 
-            //         options.UseSqlite(Configuration.GetConnectionString("ParkedVehicleContext")));
+            services.AddDbContext<ParkedVehicleContext>(options => 
+                    options.UseSqlite(Configuration.GetConnectionString("ParkedVehicleContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
